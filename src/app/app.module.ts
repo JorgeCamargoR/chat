f79fe84556cf7644e4a209ed99e3fb5d88556d1e
Kuery <developer@kuery.com.co>
2017-08-24 16:54:01 -0500

    carpeta chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+
+import { ChatPage} from '../pages/chat/chat';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+ var config = {
+    apiKey: "AIzaSyCquSnGztdUcgK3yOW2LrNulla2dTbG9bo",
+    authDomain: "chatkuery.firebaseapp.com",
+    databaseURL: "https://chatkuery.firebaseio.com",
+    projectId: "chatkuery",
+    storageBucket: "chatkuery.appspot.com",
+    messagingSenderId: "283981862542"
+  };
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage,
+
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp) ,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
